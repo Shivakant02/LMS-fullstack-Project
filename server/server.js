ec4209179599d3b8a435ca4536efae4b32e1a390
Shivakant <kants6397@gmail.com>
2024-02-10 23:07:32 +0530

    LMS completed

--- server/server.js
+++ server/server.js
@@ -1,22 +1,27 @@
-import connectToDB from './config/dbConnection.js';
+import { v2 } from 'cloudinary';
+import Razorpay from 'razorpay';
+
 import app from './app.js';
-import { config } from 'dotenv';
-import cloudinary from 'cloudinary'
-config();
+import connectToDB from './configs/dbConn.js';
 
-const PORT = process.env.PORT || 5011;
+// Cloudinary configuration
+v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Razorpay configuration
+export const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
+});
+
+const PORT = process.env.PORT || 5000;
 
 
-cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    api_key: process.env.CLOUDINARY_API_KEY,
-
-})
-
-app.listen(PORT, async() => {
-    await connectToDB();
-
-    console.log(`App is running at http://localhost:${PORT}`);
-})
-
+app.listen(PORT, async () => {
+  // Connect to DB
+  await connectToDB();
+  console.log(`App is running at http://localhost:${PORT}`);
+});
